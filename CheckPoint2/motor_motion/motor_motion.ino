f8ed924d39e57a26fbb2aa7cb14d5792db80cb1b
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2025-09-30 22:46:57 +0800

    Update motor_motion.ino

--- CheckPoint2/motor_motion/motor_motion.ino
+++ CheckPoint2/motor_motion/motor_motion.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <stdint.h>
 #include <ros.h>
-#include <std_msgs/Int32.h>
+#include <std_msgs/Float32.h>
 #include <ros_motion/pwmValue.h> // self-defined ros msg
 
 const int ENA = 6; // left wheel
@@ -16,7 +16,6 @@ const int ENCODER_R_B = 7;
 const int ENCODER_L_A = 3;
 const int ENCODER_L_B = 13;
 
-#define PWM_TO_RPM 1.25
 #define ROS_SPIN_TIME 10
 #define CPR 960.0f
 
@@ -24,6 +23,11 @@ volatile int32_t pulses_left = 0;
 volatile int32_t pulses_right = 0;
 
 volatile uint32_t ticks = 0;
+
+float rightWheelRPM = 0.0;
+float leftWheelRPM = 0.0;
+float desiredRPM_R = 0.0;
+float desiredRPM_L = 0.0;
 
 void encoder_init();
 void encoderL_ISR() { pulses_left += digitalRead(ENCODER_L_B) ? 1 : -1; }
@@ -36,7 +40,7 @@ float read_RPM_right(float cpr = CPR);
   
 void motor_control(int pwm, int inA, int inB, int enable);
 void motor_acuation(const ros_motion::pwmValue& inputValue);
-int PID_control(int input_value, int encoder_rpm);
+float PID_control(float desired_value, float measurement);
 
 ros::NodeHandle node;
 ros::Subscriber<ros_motion::pwmValue> sub("motor_pwm", &motor_acuation);
@@ -56,6 +60,7 @@ void motor_control(int pwm, int inA, int inB, int enable)
 {
     boolean isForward = pwm > 0 ? 1 : 0; 
     int pwmVal = abs(pwm);
+    pwmVal = constrain(pwmVal, 0, 255);
    
     digitalWrite(inA, isForward ? HIGH : LOW);
     digitalWrite(inB, isForward ? LOW : HIGH);
@@ -114,34 +119,64 @@ float read_RPM_right(float cpr) {
 
 void motor_acuation(const ros_motion::pwmValue& inputValue)
 {    
-    motor_control(inputValue.leftPWMValue,  IN1, IN2, ENA);
-    motor_control(inputValue.rightPWMValue, IN3, IN4, ENB);
+  desiredRPM_L = inputValue.leftPWMValue;
+  desiredRPM_R = inputValue.rightPWMValue;
 }
 
-int PID_control(int input_value, int encoder_rpm)
+float PID_control(float desired_value, float measurement)
 {
   float output = 0.0;
-  int desired_rpm = input_value * PWM_TO_RPM;  // PWM => RPM
+  static float preError = 0.0;
+  
   
   float dt = ROS_SPIN_TIME / 1000.0f;   // ros spin every 10ms
   float kp = 10, ki = 3, kd = 0.5;  // PID parameters
- 
-  static int pre_error = 0;
-  int error = desired_rpm - encoder_rpm;
   
-  static float integral = 0.0;
-  integral += error * dt;
+  float error = desired_value - measurement;
+  float threshold = 1.0;   // error within 1 RPM
   
-  static float derivative = 0.0;
-  derivative = (error - pre_error) / dt;    
+  if (abs(error) < threshold) return 0.0;
   
-  output = kp * error + ki * integral + kd * derivative;
+  float integral = 0.0;
+  float derivative = 0.0;
   
-  if (abs(output) < 10)
-    return input_value;
-  else 
-    return output / PWM_TO_RPM; // RPM => PWM
+  integral = error * dt;
+  derivative = (error - preError) / dt;
+  preError = error;
+  
+  return kp * error + ki * integral + kd * derivative;
 }
+
+//
+//float PID_control(float right_input, float left_input, float right_output, float left_output)
+//{
+//  float output = 0.0;
+//  float small_disturbance = 1e-6;
+//  
+//  right_input = max(right_input, small_disturbance);
+//  left_input = max(left_input, small_disturbance);
+//  right_output = max(right_output, small_disturbance);
+//  left_output = max(left_output, small_disturbance);
+//
+//  float dt = ROS_SPIN_TIME / 1000.0f;   // ros spin every 10ms
+//  float kp = 10, ki = 3, kd = 0.5;  // PID parameters
+// 
+//  static int pre_error = 0;
+//  int error = (right_input / left_input) - (right_output / left_output);  // PWM => RPM is not constant
+//  
+//  const float threshold = 0.15;
+//  if (abs(error) < threshold) return 0.0;
+//  
+//  static float integral = 0.0;
+//  integral += error * dt;
+//  
+//  static float derivative = 0.0;
+//  derivative = (error - pre_error) / dt;    
+//  
+//  output = kp * error + ki * integral + kd * derivative;
+//  
+//  return output;
+//}
 
 void setup()
 {
@@ -168,20 +203,15 @@ void loop()
   node.spinOnce();
   delay(ROS_SPIN_TIME);
   
-  float L_RPM = read_RPM_left(CPR);
   char fbuf[16];
-  dtostrf(L_RPM, 0, 2, fbuf);
+  dtostrf(leftWheelRPM, 0, 2, fbuf);
   char buf[48];
   snprintf(buf, sizeof(buf), "Left RPM: %s", fbuf);
   node.loginfo(buf);
   
-  float R_RPM = read_RPM_right(CPR);
   char ebuf[16];
-  dtostrf(R_RPM, 0, 2, ebuf);
+  dtostrf(rightWheelRPM, 0, 2, ebuf);
   char buf2[48];
   snprintf(buf2, sizeof(buf2), "Right RPM: %s", ebuf);
   node.loginfo(buf2);
-
-  delay(500);
 }
-
