7100b4c3129f347c93aa151a63515ff29b7f64c2
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2025-10-18 16:08:42 +0800

    Update motor_motion.h

--- CheckPoint3/Arduino/obstacle_avoidance/motor_motion.h
+++ CheckPoint3/Arduino/obstacle_avoidance/motor_motion.h
@@ -1,1 +1,42 @@
+#pragma once
+#include <Arduino.h>
+#include <stdint.h>
+
+// ---------- Motor pins ----------
+extern const int ENA; // left wheel (PWM)
+extern const int IN1;
+extern const int IN2;
+extern const int ENB; // right wheel (PWM)
+extern const int IN3;
+extern const int IN4;
+
+// ---------- Encoder pins ----------
+extern const int ENCODER_R_A;
+extern const int ENCODER_R_B;
+extern const int ENCODER_L_A;
+extern const int ENCODER_L_B;
+
+// ---------- Params ----------
+#ifndef PPR
+#define PPR 960.0f
+#endif
+#ifndef SPEED_WINDOW_MS
+#define SPEED_WINDOW_MS 500
+#endif
+
+// initialization
+void motor_encoder_begin();
+
+// read RPM
+float read_left_rpm();
+float read_right_rpm();
+
+// motor control
+void motor_control(int pwm, int inA, int inB, int enable);
+
+void move_forward(int pwm = 90);
+void move_backward(int pwm = 90);
+void turn_left(int inner_pwm = 90, int outer_pwm = 110);
+void turn_right(int inner_pwm = 90, int outer_pwm = 110);
+void stop_motors();
 
