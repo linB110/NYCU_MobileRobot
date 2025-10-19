b2678020f64e8c61c0856163868b546c72b42c4e
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2025-10-19 14:54:40 +0800

    Implement obstacle avoidance sensor functionality

---
+++ CheckPoint3/Arduino/obstacle_avoidance/obstacle_avoidance.cpp
@@ -0,0 +1,34 @@
+#include "obstacle_avoidance.h"
+
+#define EI_NOTEXTERNAL  
+#include <EnableInterrupt.h>
+
+volatile bool right_hit = false;
+volatile bool left_hit  = false;
+volatile bool end_hit   = false;
+
+void sensor_init()
+{
+  pinMode(touch_sensor_right, INPUT);
+  pinMode(touch_sensor_left, INPUT);
+  pinMode(touch_sensor_endmove, INPUT);
+  
+  enableInterrupt(touch_sensor_right, touched_right, FALLING);
+  enableInterrupt(touch_sensor_left, touched_left, FALLING);
+  enableInterrupt(touch_sensor_endmove, touched_target, FALLING);
+}
+
+void touched_right()
+{
+  right_hit = true;
+}
+
+void touched_left()
+{
+  left_hit = true;
+}
+
+void touched_target()
+{
+  end_hit = true;
+}
